872ba63398c0ff3cb3f99c335e8da31791838f9a
giosermon <2172@holbertonschool.com>
2021-07-22 19:35:30 -0500

    update betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: a pointer to the root node of the tree to measure the size
- * Return: size of binary tree or 0 if tree is NULL
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ * Return: Depth of the binary tree or 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
