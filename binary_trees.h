ad001a9b47c9ea619ad1e5b1a45ea506870b35f8
giosermon <2172@holbertonschool.com>
2021-07-24 11:43:36 -0500

    update lib

--- binary_trees.h
+++ binary_trees.h
@@ -45,12 +45,39 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_insert(bst_t **tree, int value);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void recursion(link_t **head, const binary_tree_t *tree);
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_complete(const binary_tree_t *tree);
+void _push(binary_tree_t *node, link_t *head, link_t **tail);
+void free_q(link_t *head);
+link_t *new_node(binary_tree_t *node);
+int binary_tree_is_bst(const binary_tree_t *tree);
+int check_sub_tree_Right(const binary_tree_t *node, int min);
+int check_sub_tree_Left(const binary_tree_t *node, int max);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_remove(bst_t *root, int value);
+int remove_type(bst_t *root);
+int two_children(bst_t *root);
+int successor(bst_t *node);
+int binary_tree_is_avl(const binary_tree_t *tree);
+int bal_avl(const binary_tree_t *tree, int lower, int high);
+size_t binary_tree_height(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *aux_sort(avl_t *parent, int *array, int begin, int last);
 
 
 #endif /* BINARY_TREES_H */
